1aa530858eae0ab5d24e88b6f0fa486580b6cd83
ashwinp301 <ashwin.parthasarathy2021@vitstudent.ac.in>
2021-10-01 19:18:32 +0530

    Fifth commit

---
+++ product_two_numbers.py
@@ -0,0 +1,6 @@
+def multi(a,b):
+    multiply=a*b
+    print("Product of two given numbers is:",multiply)
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+multi(num1,num2)
