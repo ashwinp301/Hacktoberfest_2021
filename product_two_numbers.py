ae6eb77b5b851fcf3356ed5a678f775a47f0a5f7
AR-Thube <91319379+AR-Thube@users.noreply.github.com>
2021-10-26 10:50:22 +0530

    Update product_two_numbers.py

--- product_two_numbers.py
+++ product_two_numbers.py
@@ -1,6 +1,13 @@
-def multi(a,b):
-    multiply=a*b
-    print("Product of two given numbers is:",multiply)
-num1=int(input("Enter first number: "))
-num2=int(input("Enter second number: "))
-multi(num1,num2)
+#Taking input from the user.
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+
+#Defining a function to calculate multiplication of the two numbers.
+def multiply(a,b):
+    result = a * b
+    print("Product of two given numbers is: ", result)
+
+    
+#Calling the function to get the desired output.
+multiply(num1, num2)
